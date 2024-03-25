2f7c69c9c8c04a75979bf1ddad880106766ff64c
ari <ari.son@h2ohospitality.io>
2024-03-25 11:39:32 +0900

    feat: 수정

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,15 @@
-const path = require("path");
+import path from "path";
+import withTM from "next-transpile-modules";
 
-/** @type {import('next').NextConfig} */
-
-// Remove this if you're not using Fullcalendar features
-const withTM = require("next-transpile-modules")([
-  // '@fullcalendar/common',
-  // '@fullcalendar/react',
-  // '@fullcalendar/daygrid',
-  // '@fullcalendar/list',
-  // '@fullcalendar/timegrid',
-]);
-
-module.exports = withTM({
+const nextConfig = {
+  // Remove this if you're not using Fullcalendar features
+  ...withTM({
+    // '@fullcalendar/common',
+    // '@fullcalendar/react',
+    // '@fullcalendar/daygrid',
+    // '@fullcalendar/list',
+    // '@fullcalendar/timegrid',
+  }),
   // distDir: 'build',
   output: "standalone",
   trailingSlash: true,
@@ -28,13 +26,10 @@ module.exports = withTM({
   },
 
   webpack(config) {
-    config.resolve = {
-      alias: {
-        "@src": path.join(__dirname, "src"),
-        "@images": path.join(__dirname, "public", "images"),
-      },
-      ...config.resolve,
-    };
+    config.resolve.alias["@src"] = path.join(__dirname, "src");
+    config.resolve.alias["@images"] = path.join(__dirname, "public", "images");
     return config;
   },
-});
+};
+
+export default nextConfig;
