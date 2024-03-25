5dfd73158175ba43b26e377d9351bc1498153cf9
ari <ari.son@h2ohospitality.io>
2024-03-25 11:45:27 +0900

    feat: 수정4

--- next.config.mjs
+++ next.config.mjs
@@ -1,38 +1,6 @@
-import path from "path";
-
 /** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+};
 
-// Remove this if you're not using Fullcalendar features
-import withTM from "next-transpile-modules";
-
-export default withTM({
-  // distDir: 'build',
-  output: "standalone",
-  trailingSlash: true,
-  reactStrictMode: false,
-  experimental: {
-    esmExternals: false,
-    externalDir: true,
-  },
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-
-  webpack(config) {
-    config.resolve = {
-      alias: {
-        "@src": path.join(new URL(".", import.meta.url).pathname, "src"),
-        "@images": path.join(
-          new URL(".", import.meta.url).pathname,
-          "public",
-          "images"
-        ),
-      },
-      ...config.resolve,
-    };
-    return config;
-  },
-});
+export default nextConfig;
