e722493d63efbd608fd44d1c641d013f6fec30dd
ari <ari.son@h2ohospitality.io>
2024-03-25 11:55:48 +0900

    feat: 수정7

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,7 @@
-/** @type {import('next').NextConfig} */
+// next.config.mjs 파일
 const nextConfig = {
   reactStrictMode: true,
   pageExtensions: ["mdx", "md", "jsx", "js", "tsx", "ts"],
 };
-
-module.exports = nextConfig;
 
 export default nextConfig;
