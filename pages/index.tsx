3803428e33beeb68b4adb9eadc67e38beb29124f
ari <ari.son@h2ohospitality.io>
2024-03-27 11:05:52 +0900

    feat: test

--- pages/index.tsx
+++ pages/index.tsx
@@ -213,6 +213,22 @@ export default function Home() {
       console.error("EXIMBAY SDK not loaded");
     }
   };
+  useEffect(() => {
+    const script1 = document.createElement("script");
+    script1.src = "https://code.jquery.com/jquery-1.12.4.min.js";
+    script1.defer = true;
+    document.body.appendChild(script1);
+
+    const script2 = document.createElement("script");
+    script2.src = "https://api-test.eximbay.com/v1/javascriptSDK.js";
+    script2.defer = true;
+    document.body.appendChild(script2);
+
+    return () => {
+      document.body.removeChild(script1);
+      document.body.removeChild(script2);
+    };
+  }, []);
   return (
     <div>
       <Script src="https://code.jquery.com/jquery-1.12.4.min.js" defer />
